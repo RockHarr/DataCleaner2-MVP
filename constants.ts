f1005512c0708c2eb2c057a1ae134120ad10fc4e
Rockwell Harrison <rockwell.harrison@gmail.com>
2025-11-20 16:16:01 -0300

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,20 @@
+export const REGION_MAP: Record<string, string> = {
+  // normalizados a upper sin acentos
+  VALPARAISO: 'Valparaíso',
+  'V A L P A R A I S O': 'Valparaíso',
+  RM: 'Metropolitana de Santiago',
+  'REGION METROPOLITANA': 'Metropolitana de Santiago',
+  METROPOLITANA: 'Metropolitana de Santiago',
+  BIOBIO: 'Biobío',
+  'BIO BIO': 'Biobío',
+  'B I O B I O': 'Biobío',
+  NUBLE: 'Ñuble',
+  ÑUBLE: 'Ñuble',
+  ARAUCANIA: 'La Araucanía',
+  ARAUcania: 'La Araucanía',
+  TARAPACA: 'Tarapacá',
+  'ARICA Y PARINACOTA': 'Arica y Parinacota',
+  AYSEN: 'Aysén del General Carlos Ibáñez del Campo',
+  MAGALLANES: 'Magallanes y de la Antártica Chilena',
+  // … agrega más según tus fuentes reales
+};
